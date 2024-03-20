e7dc8c30f4c874c5a46e640fa9ace62a6f8897e2
StellaZenith <162518869+StellaZenith@users.noreply.github.com>
2024-03-20 22:04:50 +0700

    Create highOrder_function.js
    penjelasan singkat High Order Function

---
+++ highOrder_function.js
@@ -0,0 +1,29 @@
+//Array map() merupakan salah satu contoh Higher-Order Function yang ada di JavaScript. Karena dalam penggunaanya, ia menerima satu buah argumen yang merupakan sebuah function.
+//Dengan mengetahui adanya Higher-Order Function, Anda bisa membuat fungsi map() versi Anda sendiri!
+const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
+
+const arrayMap = (arr, action) => {
+  const loopTrough = (arr, action, newArray = [], index = 0) => {
+    const item = arr[index];
+    if(!item) return newArray;
+    return loopTrough(arr, action, [...newArray, action(arr[index])], index + 1);
+  }
+
+  return loopTrough(arr, action);
+}
+
+
+const newNames = arrayMap(names, (name) => `${name}!` );
+
+console.log({
+  names,
+  newNames,
+});
+
+/**
+ * output:
+ * {
+ *   names: [ 'Harry', 'Ron', 'Jeff', 'Thomas' ],
+ *   newNames: [ 'Harry!', 'Ron!', 'Jeff!', 'Thomas!' ]
+ * }
+ */
